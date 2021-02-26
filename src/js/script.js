2285ed0b72c57158a47b86f57c0b3a2ced84d9c5
Sebastian Sikora <ssikora80@gmail.com>
2021-02-26 13:38:23 +0100

    Add initActions function work in progress

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@
 {
   'use strict';
 
+  /*Add reference */
   const select = {
     templateOf: {
       booksTemplate: '#template-book', //szablon
@@ -12,12 +13,12 @@
     },
   };
 
-  /* make a template for a book (SZABLON) */
+  /* make a Handlebars.compile for a booksTemplate (SZABLON) */
   const templates = {
     booksTemplate: Handlebars.compile(document.querySelector(select.templateOf.booksTemplate).innerHTML),
   };
   
-  /* Petla przechodząca po każdym elemencie dataSource.books z pliku data.js 10.2 Ćwiczenie 1*/
+  /* Petla przechodząca po każdym elemencie dataSource.books z pliku data.js // 10.2 Ćwiczenie 1 // */
   function render() {
     for(let books of dataSource.books) {
 
@@ -36,5 +37,26 @@
     }
   }
   render(); 
-  
+
+  /* dodajemy tablicę favoriteBooks w której będą przechowywane książki z klasą favorite // 10.2 Ćwiczenie 2 // */
+  const favoriteBooks = [];
+
+  /* tworzymy funkcje initActions odpowwidzialną za nadawanie klasy favorite */
+  function initActions() {
+    
+    const booksContainer = document.querySelector(select.containerOf.booksList);
+    const booksImage = booksContainer.querySelectorAll('.book_image')
+    
+    /* double-clicked element */
+    for(let image of booksImage) {
+        image.addEventListener('dbclick', function(event) {
+            event.preventDefault(); // zatrzymanie domyślnej akcji
+            image.classList.add('favorite'); // dodanie klasy favorite 
+            const id = image.getAttribute('data-id'); // pobranie identyfikatora książki
+            favoriteBooks.push(id);
+
+        });
+    }
+  }
+  initActions();
 }
